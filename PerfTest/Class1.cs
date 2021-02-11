58b0ef3cede0eaaac2af879f0aebd4cf26836dc3
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-02-11 11:19:05 +0300

    Add many usages.

---
+++ PerfTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerfTest
+{
+    public class Class1
+    {
+        public (string s_renamed1, int t_renamed) Test121()
+        {
+            return (null, 0);
+        }
+    }
+}
