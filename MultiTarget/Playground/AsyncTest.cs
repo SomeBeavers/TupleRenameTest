bc9d6d1e47c885951dc43787d724ec16eddd9105
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-03-24 12:11:26 +0300

    Add async test.

---
+++ MultiTarget/Playground/AsyncTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MultiTarget.Playground
+{
+    public class AsyncTest<T>
+    {
+        public async Task<(T t, List<(U u, string name)> list, (Unnamed1, Named named))> Async1<U>((T t, List<(U u, string name)> list, (Unnamed, Named named)) param = default)
+        {
+            return (default, null, (null, null));
+        }
+    }
+
+    public class UseAsyncTest<T>
+    {
+        private async Task UseAsync1()
+        {
+            var list = (await new AsyncTest<string>().Async1<T>()).list;
+        }
+    }
+
+    public class Unnamed1
+    {
+    }
+
+    public class Named
+    {
+    }
+}
